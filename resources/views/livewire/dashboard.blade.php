f52c83b76e14ce4d6e85dd41cc8627a844965754
Rizki Firmansyah <rizkifirmansyah2642@gmail.com>
2023-12-13 21:29:42 +0700

    debugging tanggal v2

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -10,7 +10,8 @@
     <div class="mt-4">
         <div class="max-w-7xl mx-auto overflow-hidden sm:rounded-lg">
             <div class="grid grid-cols-2 gap-4 mb-4 ">
-                <div class="flex flex-col justify-between max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow max-h-max">
+                <div
+                    class="flex flex-col justify-between max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow max-h-max">
                     <h5 class="text-7xl font-bold tracking-tight text-gray-900 py-3">
                         {{ $total_tagihan }}
                     </h5>
@@ -22,22 +23,19 @@
                 <div class="block max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow">
                     <div class="mx-auto my-2">
                         @if ($tagihan_terdekat)
-                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 py-3">
-                                {{ $tagihan_terdekat->nama }}
-                            </h5>
-                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 py-3">
-                                {{ $tagihan_terdekat->tanggal_tagihan }}
-                            </h5>
-                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 py-3">
-                                {{ $tagihan_terdekat->total_tagihan }}
-                            </h5>
-                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 py-3">
-                                {{ $tagihan_terdekat->status }}
-                            </h5>
-                           @else
-                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 py-3">
+                            <h2 class="mb-4 text-4xl font-bold tracking-tight text-gray-900">
+                                {{ $tagihan_terdekat->tanggal->format('d M Y') }}
+                            </h2>
+                            <p class="text-xl font-bold mb-2">
+                                {{ number_format($tagihan_terdekat->jumlah, 0, ',', '.') }}
+                            </p>
+                            <p class="text-xl">
+                                {{ $tagihan_terdekat->debitur->nama }}
+                            </p>
+                        @else
+                            <h2 class="mb-4 text-4xl font-bold tracking-tight text-gray-900">
                                 Tidak ada tagihan
-                            </h5> 
+                            </h2>
                         @endif
                     </div>
                     <p class="font-normal text-gray-500">
